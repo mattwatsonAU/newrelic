6894b154a91461c48efe7933cfb686d963bb9a4f
Matt Watson <matt@mattwatson.net>
2019-03-01 11:53:35 +1100

    getting started scripts

--- Insights/Scripts/get_dashboard.py
+++ Insights/Scripts/get_dashboard.py
@@ -6,7 +6,6 @@ apiKey='' # Admin API Key
 dashboardId='' # Dashboard ID found at the end of its URL e.g. https://insights.newrelic.com/accounts/12345/dashboards/50 dashboardID is 50
 outputFile=''+'.json' # File name for the output json file
 
-
 ## Do not modify below this line
 
 url = 'https://api.newrelic.com/v2/dashboards/'+dashboardId+'.json'
